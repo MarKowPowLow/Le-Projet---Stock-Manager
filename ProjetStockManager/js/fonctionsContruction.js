7cc15367ee482c6908a11b5513dfa4a38aa3962f
ElementaryLewis <lewis.p.mu@outlook.com>
2023-11-17 09:41:03 +0100

    Copie/colle manuallement les fichier original.

--- ProjetStockManager/js/fonctionsContruction.js
+++ ProjetStockManager/js/fonctionsContruction.js
@@ -1,35 +1,49 @@
-import { tableauObjectDeChamp } from "./test.js";
+import { tableauObjectDeChamp } from "./test.js"
 //constructeur container principal
 
 const constructeurContainerPrincipal = (text) => {
-  const containerPrincipal = document.createElement("div");
-  containerPrincipal.textContent = text;
 
-  return containerPrincipal;
-};
+    const containerPrincipal = document.createElement("div");
+    containerPrincipal.textContent = text;
+
+
+
+    return containerPrincipal;
+
+
+}
 //######   construction de la ligne enfants contenant les information saisi.#############
 
 function objetConstructeur() {
-  let obj = {};
-  let conteneurliste = document.getElementById("conteneurliste");
+    let obj = {}
+    let conteneurliste = document.getElementById("conteneurliste")
+    
 
-  for (let ob in tableauObjectDeChamp) {
-    let temp = document.getElementById(tableauObjectDeChamp[ob].nom);
-    if (temp !== null) {
-      obj[temp.id] = temp.value;
-      temp.value = "";
+    for (let ob in tableauObjectDeChamp) {
+        let temp = document.getElementById(tableauObjectDeChamp[ob].nom)
+        if (temp !== null) {
+            obj[temp.id] = temp.value
+            temp.value=""
+        }
     }
-  }
-  //console.log(obj)
-  let parentLigne = document.createElement("div");
-  parentLigne.classList.add("listeobjet");
-  conteneurliste.appendChild(parentLigne);
-  for (let temp in obj) {
-    let enfantLigne = document.createElement("div");
-    enfantLigne.textContent = obj[temp];
-    parentLigne.appendChild(enfantLigne);
-    //console.log(obj[temp])
-  }
+    //console.log(obj)
+    let parentLigne = document.createElement("div")
+    parentLigne.classList.add("listeobjet")
+    conteneurliste.appendChild(parentLigne)
+    for (let temp in obj){
+        
+        let enfantLigne = document.createElement("div")
+        enfantLigne.textContent=obj[temp]
+        parentLigne.appendChild(enfantLigne)
+        //console.log(obj[temp])
+
+    }
 }
 
-export { constructeurContainerPrincipal, objetConstructeur };
+
+
+
+
+
+export { constructeurContainerPrincipal, objetConstructeur }
+
--- ProjetStockManager/js/messageutilisateur.js
+++ ProjetStockManager/js/messageutilisateur.js
@@ -1,36 +1,40 @@
-import { fonctionBouton } from "./boutons.js";
+import { fonctionBouton } from "./boutons.js"
 
 let buttonValider = {
-  type: "div",
-  class: "bouton",
-  text: "Valider",
-};
+    type : "div",
+    class : "bouton",
+    text : "Valider"
+
+}
 let buttonAnnuler = {
-  type: "div",
-  class: "bouton",
-  text: "Annuler",
-};
+    type : "div",
+    class : "bouton",
+    text : "Annuler",
 
-const functionboutonValider = function () {
-  divprincipale.remove();
-  validation = true;
-  return validation;
-};
+}
 
-const functionboutonAnnuler = function () {
-  divprincipale.remove();
-  validation = false;
-  return validation;
-};
+const functionboutonValider = function() {
+    divprincipale.remove();
+    validation = true;
+    return validation;
 
-const messageUtilisateur = function () {
-  divprincipale = document.createElement("div");
-  divprincipale.className = "validationAbsolue";
-  divValider = fonctionBouton(buttonValider, functionboutonValider);
-  divAnnuler = fonctionBouton(buttonAnnuler, functionboutonAnnuler);
-  document.body.appendChild(divprincipale);
-  divprincipale.appendChild(divValider);
-  divprincipale.appendChild(divAnnuler);
-};
+}
 
-export { messageUtilisateur };
+const functionboutonAnnuler = function() {
+    divprincipale.remove();
+    validation = false;
+    return validation;
+}
+
+const messageUtilisateur = function (){
+    divprincipale = document.createElement("div");
+    divprincipale.className = "validationAbsolue";
+    divValider = fonctionBouton(buttonValider, functionboutonValider);
+    divAnnuler = fonctionBouton(buttonAnnuler, functionboutonAnnuler);
+    document.body.appendChild(divprincipale);
+    divprincipale.appendChild(divValider);
+    divprincipale.appendChild(divAnnuler);
+    
+}
+
+export{messageUtilisateur}

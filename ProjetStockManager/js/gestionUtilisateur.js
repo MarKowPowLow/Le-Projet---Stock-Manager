6f75b8239d0d6ebfa842b0ce838b7e664010d01e
Zaroewn <maxime.lamotte@outlook.com>
2023-11-24 14:52:57 +0100

    modif gestion utilisateur

--- ProjetStockManager/js/gestionUtilisateur.js
+++ ProjetStockManager/js/gestionUtilisateur.js
@@ -1,4 +1,4 @@
-// ---------------------------------------------------------------------------- Variables Globale -------------------------------------------------------------------------------//
+import{objetConstructeur, affichagePopUpModifObjet} from "./fonctionsContruction.js"
 
 
 // Création conteneur Top Bar
@@ -45,69 +45,73 @@ topBar.appendChild(divRecherche);
 
 
 // Création Div Inputs
+
 let divInput = document.createElement("div");
 divInput.classList.add("divinput");
 document.body.appendChild(divInput);
 
+    
+    let inputNom = document.createElement("input");
+    inputNom.setAttribute("type", "text");
+    inputNom.setAttribute("placeholder","Nom");
+    divInput.appendChild(inputNom);
 
-// Création des Input
+    let inputPrenom = document.createElement("input");
+    inputPrenom.setAttribute("type", "text");
+    inputPrenom.setAttribute("placeholder", "Prénom");
+    divInput.appendChild(inputPrenom);
 
-let tableauObjectDeChamp = [
-    {nom :"Nom", type:"text"},
-    {nom: "Prénom", type: "text"},
-    {nom:"Email",type: "email"},
-]
+    let inputEmail = document.createElement("input");
+    inputEmail.setAttribute("type", "email");
+    inputEmail.setAttribute("placeholder", "Email");
+    divInput.appendChild(inputEmail);
 
-for(let champ of tableauObjectDeChamp) {
-
-    let inputProduit = document.createElement("input");
-    inputProduit.setAttribute("type", champ.type);
-    inputProduit.setAttribute("placeholder", champ.nom);
-    divInput.appendChild(inputProduit);
-}
-
-let validButton = document.createElement("img");
-validButton.src = "./img/Cercle-valider.svg";
-validButton.className = "imagevalider";
-divInput.appendChild(validButton);
+    let validButton = document.createElement("img");
+    validButton.src = "./img/Cercle-valider.svg";
+    validButton.className = "imagevalider";
+    divInput.appendChild(validButton);
 
 // Création conteneur liste
-
 let conteneurList = document.createElement("div");
 conteneurList.classList.add("conteneurliste")
 document.body.appendChild(conteneurList);
 
-
-
 // Création écouteur événement bouton valider
-validButton.addEventListener("click", () => {
-    listeobjet = document.createElement("div");
-    listeobjet.classList.add("listeobjet");
-    conteneurList.appendChild(listeobjet);
+    validButton.addEventListener("click", () => {
+
+        if((inputPrenom.value == "") || (inputNom.value =="") || (inputEmail.value =="")) {
+            alert("Veuillez remplir les champs demandés")
+        } else {
+        let listeobjet = document.createElement("div");
+        listeobjet.classList.add("listeobjet");
+        conteneurList.appendChild(listeobjet);
+        
+        // Création des divs contenant la valeur des inputs
+        
+        let objetNom = document.createElement("div");
+        objetNom.textContent = inputNom.value;
+        listeobjet.appendChild(objetNom);
+        
+        let objetPrenom = document.createElement("div");
+        objetPrenom.textContent = inputPrenom.value;
+        listeobjet.appendChild(objetPrenom);
+        
+        let objetEmail = document.createElement("div");
+        objetEmail.textContent = inputEmail.value;
+        listeobjet.appendChild(objetEmail);
+        
+        let deleteButtonConteneurListe = document.createElement("img");
+        deleteButtonConteneurListe.src ="./img/circle-xmark-regular.svg";
+        deleteButtonConteneurListe.className = "imagesupprimer";
+        listeobjet.appendChild(deleteButtonConteneurListe);
+
+        deleteButtonConteneurListe.addEventListener("click", () => {
+            deleteButtonConteneurListe.parentNode.remove()
+        })
+    }
+    })
+
     
-    // Création des divs contenant la valeur des inputs
-   
-    objetProduit = document.createElement("div");
-    objetProduit.textContent = inputProduit.value;
-    listeobjet.appendChild(objetProduit);
-
-    objetReference = document.createElement("div");
-    objetReference.textContent = inputReference.value;
-    listeobjet.appendChild(objetReference);
-
-    objetQuantite = document.createElement("div");
-    objetQuantite.textContent = inputQuantite.value;
-    listeobjet.appendChild(objetQuantite);
-
-    objetCategorie = document.createElement("div");
-    objetCategorie.textContent = inputCategorie.value;
-    listeobjet.appendChild(objetCategorie);
-
-    deleteButtonConteneurListe = document.createElement("img");
-    deleteButtonConteneurListe.src ="./img/circle-xmark-regular.svg";
-    deleteButtonConteneurListe.className = "imagesupprimer";
-    listeobjet.appendChild(deleteButtonConteneurListe);
-})
 
 
 
